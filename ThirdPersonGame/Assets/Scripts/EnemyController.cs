ee2ac5b59f4ac064d3c3b4797abbc2479f135717
Igor <52105996+IgorPalii@users.noreply.github.com>
2021-07-05 17:25:54 +0300

    Initial commit

---
+++ ThirdPersonGame/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    private int health;
+    private float prevHitTime, ignoreDamageWindow;
+    private Animator animator;
+
+    private NavMeshAgent agent; //объект для компонента 
+    private Transform playerT; //компонент трансформ игрока
+    //время предыдущей атаки, сколько ждать до следующей
+    private float prevAttackTime, pauseAttackWindow;
+    [SerializeField] //точки по которым враг патрулирует 
+    private Transform[] patrolTargets;
+    //индекс точки, к которой нужно двигаться
+    private int currentTargetIndex = 0; 
+    public bool isAttacking = false;//атакует ли враг
+
+    private void Start(){
+        agent = GetComponent<NavMeshAgent>();
+        playerT = GameObject.Find("Character").transform;
+        pauseAttackWindow = 2.5f;
+
+        animator = GetComponent<Animator>();
+        health = 3;
+        prevHitTime = 0f;
+        ignoreDamageWindow = 1.5f;
+    }
+
+    private void Update() {
+        isAttacking = animator.GetCurrentAnimatorStateInfo(0).IsName("Sword_Attack_R");
+        if (health > 1) {
+            float distanceToPlayer = Vector3.Distance(transform.position, playerT.position);
+            if (distanceToPlayer < 2.5f) {
+                Attack();
+            }
+            else if (distanceToPlayer > 30f) {
+                PatrolBehaviour();
+            }
+            else {
+                MoveToPlayer();
+            }            
+        }
+    }
+
+    private void OnTriggerEnter(Collider col){
+        if (col.gameObject.tag == "Weapon" 
+        && playerT.gameObject.GetComponent<PlayerController>().isAttacking 
+        && Time.time > prevHitTime + ignoreDamageWindow) {
+            health--;
+            prevHitTime = Time.time;
+            if(health > 1){
+                animator.Play("KnockdownRight");
+            }
+            else if(health == 1){
+                animator.Play("Sword_Defeat_2_Start");
+            }
+            else{
+                animator.SetTrigger("isDead");
+            }
+        }
+    }
+
+    private void CheckNewPatrolTarget() {
+        Vector3 targetPos = patrolTargets[currentTargetIndex].position;
+        if (Vector3.Distance (transform.position, targetPos) < 0.5f) {
+            if (currentTargetIndex < patrolTargets.Length -1) {
+                currentTargetIndex++;//если индекс не последний в масиве
+            }                        //устанавливаем индекс на 1 больше
+            else {
+                currentTargetIndex = 0;//иначе 0
+            }
+        }
+    }
+
+    private void MoveToPlayer()
+    {
+        animator.SetBool("isWalk", true);
+        agent.destination = playerT.position;
+    }
+
+    private void PatrolBehaviour()
+    {//если есть точки патрулирования в массиве
+        if (patrolTargets.Length > 0)
+        {
+            animator.SetBool("isWalk", true);//включаем анимацию ходьбы
+            agent.destination = patrolTargets[currentTargetIndex].position;
+            CheckNewPatrolTarget();
+        }
+    }
+
+    private void Attack() {
+        animator.SetBool("isWalk", false);//отключаем анимацию ходьбы
+        agent.destination = transform.position;
+        transform.LookAt(playerT.position);//поварачиваем к игроку лицом
+        if (Time.time > prevAttackTime + pauseAttackWindow
+            && !animator.GetCurrentAnimatorStateInfo(0).IsName("KnockdownRight"))
+        {
+            animator.Play("Sword_Attack_R");
+            prevAttackTime = Time.time;
+        }
+    }
+}
---
+++ ThirdPersonGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{   //угол поворота, направление движения, сила прыжка, скорость поворота
+    private float angleY, dirZ, jumpForce = 6f, turnSpeed = 150f;
+    private bool isGrounded; //на земле или нет
+    private Rigidbody rb;
+    private Animator animator;
+    private Vector3 jumpDir; //направление прыжка
+
+    private GameObject sword, shield;//для хранения объектов
+    private float weaponChangeTime, timeNeedToWait;
+    private bool needSetWeaponActivity;
+
+    public bool isAttacking = false;
+    private GameObject enemy;
+
+    [SerializeField]
+    private Joystick joystickRight, joystickLeft;
+
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
+        sword = GameObject.Find("CurrentSword");
+        shield = GameObject.Find("CurrentShield");
+        weaponChangeTime = 0f;
+        needSetWeaponActivity = false;
+        //Cursor.visible = false;
+
+        enemy = GameObject.Find("Enemy");
+    }
+
+    void FixedUpdate(){
+        angleY = joystickLeft.Horizontal * turnSpeed * Time.fixedDeltaTime;
+        dirZ = joystickRight.Vertical; //Input.GetAxis("Vertical"); 
+        transform.Rotate(new Vector3(0f, angleY, 0f)); //вращение игрока
+    }
+
+    private void Update(){
+        isAttacking = animator.GetCurrentAnimatorStateInfo(0).IsName("Sword_Attack_R");
+        if (isGrounded){            
+            animator.SetTrigger("isLanded");            
+            Move(dirZ, "isWalkForward", "isWalkBack");
+            Sprint();
+            Dodge();      
+            SetWeaponActivity();
+        }
+        else{
+            MoveInAir();
+        }
+    }
+
+    private void Move(float dir, string parametrName, string altParametrName){
+        if (dir > 0.3f){
+            animator.SetBool(parametrName, true);
+        }
+        else if (dir < -0.3f){
+            animator.SetBool(altParametrName, true);
+        }
+        else{
+            animator.SetBool(parametrName, false);
+            animator.SetBool(altParametrName, false);
+        }
+    }
+
+    private void Dodge(){
+        if (joystickRight.Horizontal < -0.8f){
+            animator.Play("Sword_Dodgle_Left");
+        }
+        else if (joystickRight.Horizontal > 0.8f){
+            animator.Play("Sword_Dodge_Right");
+        }
+    }
+
+    private void Sprint(){
+        if (joystickRight.Vertical > 0.9f || joystickRight.Vertical < -0.9f){
+            animator.SetBool("isRun", true);
+        }
+        else {
+            animator.SetBool("isRun", false);
+        }     
+    }
+
+    public void Jump(){
+        if (isGrounded){
+            animator.Play("JumpStart");
+            animator.applyRootMotion = false; //откл RootMotion
+            jumpDir = new Vector3(0f, jumpForce, dirZ * jumpForce / 2f);
+            jumpDir = transform.TransformDirection(jumpDir);
+            rb.AddForce(jumpDir, ForceMode.Impulse);
+            isGrounded = false; //указываем, что уже не на земле
+        }
+    }
+
+    private void MoveInAir(){
+        if (new Vector2(rb.velocity.x, rb.velocity.z).magnitude < 1.1f){
+            jumpDir = new Vector3(0f, rb.velocity.y, dirZ);
+            //направляем вектор вперед по соотношению игрока, а не сцены
+            jumpDir = transform.TransformDirection(jumpDir);            
+            rb.velocity = jumpDir;
+        }  
+    }
+
+    public void PlayEquipeAnimation(){
+        string stateName = "";
+        float waitTime = 0f;
+        if (sword.activeSelf){
+            stateName = "Sword_Holster";
+            waitTime = 0.5f;
+        }
+        else if (!sword.activeSelf){
+            stateName = "Sword_Equip";
+            waitTime = 0.2f;
+        }
+        animator.Play(stateName);//проигрываем анимацию
+        weaponChangeTime = Time.time;//записываем время дейстия
+        timeNeedToWait = waitTime;//указываем через сколько вызвать SetActive
+        needSetWeaponActivity = true;//указываем, что теперь можем вызывать SetActive
+    }
+
+    private void SetWeaponActivity()
+    {
+        if(needSetWeaponActivity && (Time.time > weaponChangeTime + timeNeedToWait))
+        {
+            sword.SetActive(!sword.activeSelf);
+            needSetWeaponActivity = false;
+        }
+    }
+
+    public void Attack()
+    {
+        if (sword.activeSelf && isGrounded)
+        {
+            animator.Play("Sword_Attack_R");
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {        
+        isGrounded = true;
+        animator.applyRootMotion = true;               
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Weapon"
+            && enemy.GetComponent<EnemyController>().isAttacking)
+        {
+            animator.Play("Sword_Hit_L_2");
+        }
+    }
+}
